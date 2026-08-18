357220e78b0d631f974d1a25a0de6d9ab0442f3d
ehsanpanhyar <muhammadehsanpanhyar@gmail.com>
2026-08-19 00:22:50 +0500

    Time: 98 ms (5.84%), Space: 47.2 MB (41.23%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,7 +4,7 @@ class Solution {
        
       for(int i=0; i<nums.length; i++){
         for(int j=0; j<nums.length; j++){
-            if(nums[i] + nums[j] == target && i!=j){
+            if(nums[i] + nums[j] == target && i != j){
                 index[0] = i;
                 index[1] = j;
                 break;
